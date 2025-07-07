b2c642a1fb0c4644c29d7d6e12215d0c7906e01c
tobo37 <gerritverse@gmail.com>
2025-07-07 19:22:14 +0200

    fix sysinfo imports

--- src-tauri/src/importer.rs
+++ src-tauri/src/importer.rs
@@ -1,5 +1,5 @@
 use serde::Serialize;
-use sysinfo::{DiskExt, SystemExt};
+use sysinfo::Disks;
 
 #[derive(Serialize)]
 pub struct ExternalDevice {
@@ -10,12 +10,10 @@ pub struct ExternalDevice {
 
 #[tauri::command]
 pub fn list_external_devices() -> Result<Vec<ExternalDevice>, String> {
-    let mut sys = sysinfo::System::new();
-    sys.refresh_disks_list();
-    sys.refresh_disks();
+    let mut disks = Disks::new_with_refreshed_list();
 
     let mut result = Vec::new();
-    for disk in sys.disks() {
+    for disk in disks.list() {
         if disk.is_removable() {
             let name = disk.name().to_string_lossy().into_owned();
             let path = disk.mount_point().to_string_lossy().into_owned();

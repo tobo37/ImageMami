54934ae2fc27e7aec674e4c984b8c9280d0e5706
tobo37 <gerritverse@gmail.com>
2025-07-16 23:29:51 +0200

    Add duplicate destination setting

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,16 +1,28 @@
-import { defineStore } from "pinia";
-import { ref, watch } from "vue";
+import { defineStore } from 'pinia';
+import { ref, watch } from 'vue';
 
-export const useSettingsStore = defineStore("settings", () => {
+export const useSettingsStore = defineStore('settings', () => {
   const importDestination = ref<string | null>(
-    localStorage.getItem("importDest"),
+    localStorage.getItem('importDest'),
+  );
+
+  const duplicateDestination = ref<string | null>(
+    localStorage.getItem('duplicateDest'),
   );
 
   watch(importDestination, (val) => {
     if (val) {
-      localStorage.setItem("importDest", val);
+      localStorage.setItem('importDest', val);
     } else {
-      localStorage.removeItem("importDest");
+      localStorage.removeItem('importDest');
+    }
+  });
+
+  watch(duplicateDestination, (val) => {
+    if (val) {
+      localStorage.setItem('duplicateDest', val);
+    } else {
+      localStorage.removeItem('duplicateDest');
     }
   });
 
@@ -18,5 +30,14 @@ export const useSettingsStore = defineStore("settings", () => {
     importDestination.value = path;
   }
 
-  return { importDestination, setImportDestination };
+  function setDuplicateDestination(path: string | null) {
+    duplicateDestination.value = path;
+  }
+
+  return {
+    importDestination,
+    setImportDestination,
+    duplicateDestination,
+    setDuplicateDestination,
+  };
 });
